3fe9a691aaba17f43f680d929ad6dab088a4f5be
RachmadFidhani <simokart3@gmail.com>
2025-02-11 10:04:50 +0700

    add icon and change title

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Retail Worth Sharing</title>
+    <title>BrandStage</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/icon/favicon-32x32.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <!-- Font Awesome for social icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
